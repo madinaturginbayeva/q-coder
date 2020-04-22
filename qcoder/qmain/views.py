86b28bb7b638af69767204fc1bc1916700e934c0
Nargiza Nassibullina <nssbllnnr>
2020-04-22 20:33:28 +0600

    add register to landing

--- qcoder/qmain/views.py
+++ qcoder/qmain/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Course
+from django.contrib import messages
+from users.forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, Group
 
 def main(request):
-    return render(request, 'qmain/landing.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            group = form.cleaned_data['group']
+            user.groups.add(group)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('/')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'qmain/landing.html', {'form': form})
 
 def courses(request):
     context = {
