c696ce5074c4bb5bd31ac529a667bb8cf58d8fe7
Nargiza Nassibullina <nssbllnnr>
2020-05-07 16:02:43 +0600

    db change

--- qcoder/qmain/models.py
+++ qcoder/qmain/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from users.models import Student, Teacher
 
 class Course(models.Model):
     SEMESTERS = [
@@ -12,14 +13,27 @@ class Course(models.Model):
     term = models.CharField(max_length=32, choices=SEMESTERS)
     year = models.IntegerField()
     entry_code = models.CharField(max_length=16)
-    lector = models.ForeignKey(User, on_delete=models.CASCADE)
+    lector = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
     
     def __str__(self):
         return self.name
 
-# class Assignment(models.Model):
-#     title = models.CharField(max_length=256)
-#     description = models.TextField()
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+class Task(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class TaskLinks(models.Model):
+    link = models.CharField(max_length=4096) 
+    task_file = models.ForeignKey(Task, on_delete=models.CASCADE) 
+
+class Assignments(models.Model):
+    grade = models.FloatField(default=0.0, null=True)
+
+class AssignmentLinks(models.Model):
+    link = models.CharField(max_length=4096) 
+    task_file = models.ForeignKey(Assignments, on_delete=models.CASCADE) 
     
     
--- qcoder/users/models.py
+++ qcoder/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Student(User):
+    student_id = models.IntegerField(null=True)
+    faculty = models.CharField(max_length=4096, null=True)
+    major = models.CharField(max_length=4096, null=True)
+    graduation_year = models.IntegerField(null=True)
+    enrollment_year = models.IntegerField(null=True)
+    phone = models.CharField(max_length=11, null=True)
+
+
+class Teacher(User):
+    faculty =  models.CharField(max_length=4096, null=True)
+    degree =  models.CharField(max_length=4096, null=True) 
+    position =  models.CharField(max_length=4096, null=True)
+    
+
+
